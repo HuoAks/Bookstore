479ed1ccb1b3d179e918493d49447abd8cebc852
HuoAks <ahuovi@gmail.com>
2020-09-23 15:05:32 +0300

    Delete and create added

--- src/main/java/hh/swd20/Bookstore/webcontroller/BookController.java
+++ src/main/java/hh/swd20/Bookstore/webcontroller/BookController.java
@@ -2,11 +2,13 @@ package hh.swd20.Bookstore.webcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import hh.swd20.Bookstore.domain.BookRepository;
+import hh.swd20.Bookstore.domain.Book;
 
 @Controller
 public class BookController {
@@ -26,5 +28,23 @@ public class BookController {
 			
 			return "booklist";
 		
+		}
+		// Delete a book
+		@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+		public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+			bookRepository.deleteById(bookId);
+			return "redirect:../booklist";
+		}
+		// Add a book
+		@RequestMapping(value = "/add")
+		public String addBook(Model model) {
+			model.addAttribute("book", new Book());
+			return "addbook";
+		}
+		// Save a book
+		@RequestMapping(value = "/save", method = RequestMethod.POST)
+		public String saveBook(Book book) {
+			bookRepository.save(book);
+			return "redirect:booklist";
 		}
 }
