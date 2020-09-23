24dafc982c9aaec835037bdba7a5818208d67dee
HuoAks <ahuovi@gmail.com>
2020-09-23 13:19:43 +0300

    Booklist thymeleaf-template

--- src/main/java/hh/swd20/Bookstore/webcontroller/BookController.java
+++ src/main/java/hh/swd20/Bookstore/webcontroller/BookController.java
@@ -1,9 +1,12 @@
 package hh.swd20.Bookstore.webcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import hh.swd20.Bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
@@ -11,6 +14,17 @@ public class BookController {
 		@RequestMapping(value = "/index", method = RequestMethod.GET)
 		public String getIndex(Model model) {
 		return "index";
+		
+		}
+		
+		@Autowired
+		private BookRepository bookRepository;
+		
+		@RequestMapping(value = "/booklist", method = RequestMethod.GET)
+		public String getBooks(Model model) {
+			model.addAttribute("books", bookRepository.findAll());
+			
+			return "booklist";
 		
 		}
 }
